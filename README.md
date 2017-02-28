5148feb51068fefab5d0b84e1a7ff36859d57072
fractalwizz <fractalwizz@hotmail.com>
2017-02-28 16:16:37 -0600

    Commit #2: BF-to-BL Translation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## BrainLollerInterpreter
 Interpreter for the Brainloller language written in Perl<br>
-V0.8
+V0.85
 
 ### Disclaimer
 Fractalwizz is not the author of any of the example programs.<br>
@@ -9,28 +9,25 @@ They are only provided to test the interpreter's functionality
 ### Module Dependencies
 Modern::Perl<br>
 GD<br>
+POSIX<br>
 File::Basename
 
 ### Usage
-perl bl.pl inputImage<br>
+perl bl.pl inputImage OR progfile<br>
   inputImage: path of Image
+  progfile: path of bf program file
   
 ie:<br>
 perl bl.pl ./Examples/hello.png<br>
-perl bl.pl catlong.png
+perl bl.pl catlong.png<br>
+perl bl.pl interpreter.bf
 
 ### Features
 BrainLoller Esoteric Programming Language<br>
 Actually just A BrainLoller-to-Brainfck converter<br>
 Processes image into a program String<br>
-which gets passed into the Brainfck interpreter to execute
-
-### TODO
-Cmd parameter for debug information<br>
-Cmd parameter for trace output for each step<br>
-Convert Brainfck code to image (?)<br>
-Cmd parameter for width of image to convert BF code (?)<br>
-Come up with more TODO items
+which gets passed into the Brainfck interpreter to execute<br>
+New in V0.85: Translate Brainfck programs to Brainloller
 
 ### License
 MIT License<br>
--- bl.pl
+++ bl.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 use Modern::Perl;
 use GD;
+use POSIX qw/ceil/;
 use File::Basename;
+no warnings 'experimental::smartmatch';
 
 # color palette
 our %colors = (
@@ -10,9 +12,11 @@ our %colors = (
                 "0000FF" => ".", "000080" => ",",
                 "FFFF00" => "[", "808000" => "]",
                 "00FFFF" => "c", "008080" => "cc",
+                "000000" => "b",
               );
 our @dp = qw/r d l u/;
 our @list;
+our @prog;
 
 my $dpval = 0;
 my ($cy, $cx) = (0,0);
@@ -23,6 +27,7 @@ my $dir;
 my $char;
 
 my $im;
+our $tr;
 
 my $image = shift;
 
@@ -30,16 +35,21 @@ if (!$image) {
     my $prog = basename($0);
     
     print "USAGE\n";
-    print "  $prog [options] imagefile\n\n";
+    print "  $prog imagefile OR progfile\n\n";
     print "DESCRIPTION\n";
     print "  BrainLoller Interpreter written in Perl\n\n";
     print "OPTIONS\n";
     print "OPERANDS\n";
-    print "  imagefile  path to input image file\n\n";
+    print "  imagefile  path to input image file\n";
+    print "  or\n";
+    print "  progfile  Brainfuck program to be translated\n\n";
     print "FILES\n";
+    print "  Output files written to current directory\n";
+    print "  Translation filename is progfile-bf.png\n\n";
     print "EXAMPLES\n";
     print "  $prog ./Examples/cat.png\n";
-    print "  $prog hello.png";
+    print "  $prog hello.png\n";
+    print "  $prog fizzbuzz.bf";
     
     exit(1);
 }
@@ -48,6 +58,18 @@ if ($image =~ m/\S+\.png$/i) {
     $im = newFromPng GD::Image($image);
 } elsif ($image =~ m/\S+\.gif$/i) {
     $im = newFromGif GD::Image($image);
+} elsif ($image =~ m/\S+\.bf$/i) {
+    @prog = convert($image);
+    
+    print "Codel size (in px): ";
+    chomp(my $cod = <>);
+    print "Image Width (in codel): ";
+    chomp(my $wid = <>);
+    
+    translate($cod, $wid);
+    endTrans($image);
+    
+    exit(0);
 } else {
     print "Error: Unsupported Image Format\n";
     exit(1);
@@ -56,7 +78,7 @@ if ($image =~ m/\S+\.png$/i) {
 my ($w, $h) = $im->getBounds();
 
 # create 2D list of colors
-extractcolors($im, $w, $h);
+extractcolors($im, $w, $h, codelsize($im, $w, $h));
 
 # clean list
 sanitize();
@@ -81,7 +103,7 @@ while ($bail) {
     ($cy, $cx) = getNext($cy, $cx, $dir);
 }
 
-#print "Result is : $program\n";
+print "Result is : $program\n";
 
 system ("perl bf.pl \"$program\"");
 
@@ -92,6 +114,40 @@ system ("perl bf.pl \"$program\"");
 #----------------------------
 
 ##\
+ # Calculates codel size of input image file
+ #
+ # param: $im: GD image
+ # param: $w:  Image width
+ # param: $h:  Image height
+ #
+ # return: codel size
+ #/
+sub codelsize {
+    my ($im, $w, $h) = @_;
+    my $store = 0;
+    my $count;
+
+    foreach my $y (0 .. $h - 1) {
+        $count = 1;
+        
+        foreach my $x (0 .. $w - 1) {
+            my $first = $im->getPixel($x, $y);
+            my $second = $im->getPixel($x + 1, $y);
+            
+            if ($first == $second) {
+                $count++;
+            } else {
+                if (!$store || $count <= $store) { $store = $count; }
+                $count = 1;
+            }
+        }
+        
+        if ($store == 1) { last; }
+    }
+    return $store;
+}
+
+##\
  # Acquires color information from image and stores in 2D list
  #
  # param: $im:   GD image
@@ -99,11 +155,11 @@ system ("perl bf.pl \"$program\"");
  # param: $h:    Image height
  #/
 sub extractcolors {
-    my ($im, $w, $h) = @_;
+    my ($im, $w, $h, $size) = @_;
     
-    for (my $x = 0; $x < $w; $x++) {
-        for (my $y = 0; $y < $h; $y++) {
-            $list[$y][$x] = rgbtohex($im->rgb($im->getPixel($x,$y)));
+    for (my $x = 0; $x < $w; $x+= $size) {
+        for (my $y = 0; $y < $h; $y+= $size) {
+            $list[$y / $size][$x / $size] = rgbtohex($im->rgb($im->getPixel($x,$y)));
         }
     }
 }
@@ -168,6 +224,121 @@ sub getNext {
     }
     
     return ($y, $x);
+}
+
+#----------------------------
+#--------Translation---------
+#----------------------------
+
+##\
+ # Collects all program instructions into array
+ #
+ # param: $file: name of input file
+ #
+ # return: @out: array of program instructions
+ #/
+sub convert {
+    my ($file) = @_;
+    my @out;
+    
+    open (FILE, '<', $file) or die("Can't open $file: $!\n");
+    while (<FILE>) {
+        my $temp = $_;
+        for (0 .. length($temp) - 1) {
+            my $char = substr($temp, 0, 1);
+            $temp = substr($temp, 1);
+                
+            if ($char =~ m/[\>\<\+\-\.\,\[\]]/) { push(@out, $char); }
+        }
+    }
+    close (FILE);
+    
+    return @out;
+}
+
+##\
+ # Translates Brainfuck program into Brainloller image
+ #
+ # param: $cod: size of codels in image
+ # param: $width: codel width of image
+ #/
+sub translate {
+    my ($cod, $width) = @_;
+    
+    my $w = $cod * $width;
+    my $h = $cod * ceil(scalar(@prog) / ($width - 2));
+    
+    my $proptr = 0;
+    my $x = 0;
+    my $y = 0;
+    my $dpv = 0;
+    my $dir = $dp[$dpv];
+    
+    $tr = new GD::Image($w, $h);
+    $tr->filledRectangle(0, 0, $w, $h, $tr->colorResolve(getColor('b')));
+    my $color;
+    
+    my @tsil;
+    while (1) {
+        if ($proptr == scalar @prog) { last; }
+        my $c = $prog[$proptr];
+        
+        if ($x == 0 && $y > 0) {
+            $tsil[$y][$x] = "cc";
+            $dpv = ($dpv - 1) % @dp;
+        } elsif ($x == $width - 1) {
+            $tsil[$y][$x] = "c";
+            $dpv = ($dpv + 1) % @dp;
+        } else {
+            $tsil[$y][$x] = $c;
+            $proptr++;
+        }
+        
+        $dir = $dp[$dpv];
+        ($y, $x) = getNext($y, $x, $dir);
+    }
+    
+    # Separate for rectangle-drawing issues
+    for my $a (0 .. @tsil - 1) {
+        for my $b (0 .. @{$tsil[0]} - 1) {
+            if (defined $tsil[$a][$b]) {
+                $color = $tr->colorResolve(getColor($tsil[$a][$b]));
+            } else {
+                $color = $tr->colorResolve(getColor('b'));
+            }
+            $tr->filledRectangle($b * $cod, $a * $cod, ($b + 1) * $cod, ($a + 1) * $cod, $color);
+        }
+    }
+}
+
+##\
+ #
+ #/
+sub endTrans {
+    my ($image) = @_;
+    
+    $image =~ /(\S+)\./;
+    $image = $1 . "-bf.png";
+    
+    open (OUT, '>', $image) or die ("Can't create $image: $!\n");
+    binmode (OUT);
+    print OUT $tr->png;
+    close (OUT);
+}
+
+##\
+ #
+ #/
+sub getColor {
+    my ($char) = @_;
+    
+    for my $i (keys %colors) {
+        if ($colors{$i} eq $char) {
+            return hextorgb($i);
+        }
+    }
+    
+    return hextorgb("000000");
 }
 
 #----------------------------
@@ -240,7 +411,7 @@ sub valid {
     my ($y, $x) = @_;
     my $out = 1;
     
-    if ($y < 0 || $y >= @list || $x < 0 || $x >= @{$list[0]}) {
+    if ($y < 0 || $y >= @list || $x < 0 || $x >= @{$list[0]} || $list[$y][$x] ~~ "000000") {
         $out = 0;
     }
     
